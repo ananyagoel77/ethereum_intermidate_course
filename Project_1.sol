cdda79f5e80d58a155647ddf3b3def3a8db68aae
ananyagoel77 <tiyagoel078@gmail.com>
2024-06-26 15:22:13 +0530

    Update Project_1.sol

--- Project_1.sol
+++ Project_1.sol
@@ -2,33 +2,37 @@
 pragma solidity ^0.8.26;
 
 contract RequireAssertRevert {
-    address private owner;
-    mapping(address => uint) private balances;
+ // State variable to store the owner's address
+    address private owner;      
+ // Mapping to store balances of addresses
+    mapping(address => uint) private balances;   
 
     constructor() {
         owner = msg.sender;
     }
-
+     // Function to deposit funds into the contract
     function deposit(uint _amount) public {
         // Require that the sender is the owner
         require(msg.sender == owner, "Only the owner can deposit funds");
-        // Increase the balance
+        // Increasing the balance of sender
         balances[msg.sender] += _amount;
     }
 
+   // Function to withdraw funds from the contract
     function withdraw(uint _amount) public {
         // Assert that the balance is sufficient
         assert(balances[msg.sender] >= _amount);
-        // Decrease the balance
+        // Decreasing the balance of the sender
         balances[msg.sender] -= _amount;
     }
 
+   // Function to transfer funds to another address
     function transfer(address _recipient, uint _amount) public {
         // Revert if the recipient is the zero address
         if (_recipient == address(0)) {
             revert("Cannot transfer to the zero address");
         }
-        // Ensure sender has sufficient balance
+        // Ensure sender has sufficient balance in account to transter the fund to another account
         require(balances[msg.sender] >= _amount, "Insufficient balance");
         // Decrease the sender's balance
         balances[msg.sender] -= _amount;
@@ -36,6 +40,7 @@ contract RequireAssertRevert {
         balances[_recipient] += _amount;
     }
 
+   // Function to get the balance of a specific account
     function getBalance(address _account) public view returns (uint) {
         return balances[_account];
     }
